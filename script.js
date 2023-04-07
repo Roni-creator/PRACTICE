5c243b0ae065e0825afd1c6ca15969b24386dbfa
veronicahmbuvi <mbuviveronicah3@gmail.com>
2023-04-06 23:32:54 -0700

    final commit

--- script.js
+++ script.js
@@ -3,33 +3,47 @@ const author=document.getElementById("author");
 const tweetButton=document.getElementById("tweet");
  let soundBtn=document.getElementById("sound");
  let QuoteForm = document.getElementById("QuoteForm");
+ function createNewQuote(e) {
+    e.preventDefault();
+    let quotename = document.getElementById("Quote");
+    let AuthorName = document.getElementById("Author");
+const quoteObj = {
+    quote: quotename.value,
+    author:AuthorName.value,
+}
+fetch(`http://localhost:3000/quotes`,{
+    method:'POST',
+    headers:{
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify(quoteObj)
+})
+.then(res => res.json())
+.then(()=>{
+    quotename.value=""
+    AuthorName.value=""
+    fetch(`http://localhost:3000/quotes`)
+    .then(res => res.json())
+    .then(data => getNewQuote(data))
+})
+}
+QuoteForm.addEventListener("submit", createNewQuote)
 
- QuoteForm.addEventListener("submit", (e) => {
-   e.preventDefault();
+
+//  QuoteForm.addEventListener("submit", (e) => {
+//    e.preventDefault();
  
-   let quotename = document.getElementById("Quote");
-   let AuthorName = document.getElementById("Author");
+   
  
-   if (quotename.value == "" || AuthorName.value == "") {
-     alert("Ensure you input a value in both fields!");
-   } else {
-     // perform operation with form input
-     alert("This Quote has been successfully Added!");
-      quotename.value = "";
-     AuthorName.value = "";
-   }
- });
-    function addQuote(quotes){
-        fetch(`http://localhost:3000/quotes)`,{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify(quotes)
-        })
-        .then(res => res.json())
-        .then(data =>console.log(data))
-    }
+//    if (quotename.value == "" || AuthorName.value == "") {
+//      alert("Ensure you input a value in both fields!");
+//    } else {
+//      // perform operation with form input
+//      alert("This Quote has been successfully Added!");
+//       quotename.value = "";
+//      AuthorName.value = "";
+//    }
+//  })
 const getNewQuote = async () =>
 {
     //api for quotes
@@ -55,3 +69,13 @@ soundBtn.addEventListener("click",()=>{
     let utterance = new SpeechSynthesisUtterance(`${text.innerHTML}`);
     speechSynthesis.speak(utterance); //speak method of speechSynthesis speaks the utterance
 })
+
+
+
+
+
+// function addQuote(quotes){
+   
+//     .then(res => res.json())
+//     .then(data =>console.log(data))
+// }
